49ab1a9b7a8064aac3e2bd4b17f5a2112d6e38f9
kamerado <kamerado9@gmail.com>
2023-09-19 10:33:14 -0400

    init

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from pytube import YouTube
-import youtubedl
 import os
 from moviepy.editor import *
+from io import StringIO
+
+def showstreams(yt):
+    result = yt.streams.filter(progressive=True, file_extension='mp4')
+    return result
 
 def selectSong():
     while True:
@@ -11,27 +15,32 @@ def selectSong():
         filename = f'Videos\{title}'
         title2 = yt.title + '.mp3'
         print(title)
-        a = input('correct?')
-        print(youtubedl.showstreams(yt))       
-        itag = input('\nSelect itag: \n')
-        try:
-            stream = yt.streams.get_by_itag(itag)
-            stream.download('Videos')
-        except Exception:
-            print('Try another url: ')
-            continue  
+        streams = f'{showstreams(yt)}'
+        streams = streams.split(',')
+        for i in streams:
+            if 'itag="22"' in i:
+                itag = 22
+                stream = yt.streams.get_by_itag(itag)
+                stream.download('Videos')
+            elif 'itag="18"' in str(i):
+                stream = yt.streams.get_by_itag(18)
+                stream.download('Videos')
+            else:
+                print('nada') 
         video = VideoFileClip(os.path.join("Videos",title))
         video.audio.write_audiofile(os.path.join("Videos",title2))
         video.close()
         os.remove(filename)
+        break
+
 
 if __name__ == '__main__':
-    ine = ''
-    title = ''
-    while ine != 'x':
+    while True:
         ine = input('S to start, X to quit.').lower()
         if ine == 's':
-            title = selectSong()
+            song = selectSong()
+        elif ine == 'x':
+            exit()
         else:
             print('wrong input.')
             continue
